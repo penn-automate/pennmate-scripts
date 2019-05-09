09a79080fe3da9e1502f004df37b4c3a8ea3d74c
SuperFashi <admin@superfashi.com>
2019-05-09 15:54:41 -0400

    update to use opendata api

--- cache_course.go
+++ cache_course.go
@@ -3,36 +3,14 @@ package main
 import (
 	"database/sql"
 	"encoding/json"
-	"fmt"
 	"log"
-	"net/http"
 	"time"
 
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/penn-automate/penn-opendata-api"
 )
 
-type CourseSearch struct {
-	ResultData  []json.RawMessage `json:"result_data"`
-	ServiceMeta ServiceMeta       `json:"service_meta"`
-}
-type ResultData struct {
-	Activity    string `json:"activity"`
-	Credits     string `json:"credits"`
-	Instructors []struct {
-		Name      string `json:"name"`
-		SectionID string `json:"section_id"`
-		Term      string `json:"term"`
-	} `json:"instructors"`
-	MaxEnrollment string `json:"max_enrollment"`
-	//MaximumCredit string `json:"maximum_credit"`
-	SectionID    string `json:"section_id"`
-	SectionTitle string `json:"section_title"`
-	Term         string `json:"term"`
-}
-
 const (
-	courseSearchUrl = `https://esb.isc-seo.upenn.edu/8091/open_data/course_section_search`
-
 	maxRetry  = 5
 	waitRetry = time.Second * 3
 )
@@ -48,50 +26,31 @@ func init() {
 }
 
 func main() {
-	request, e := http.NewRequest("GET", courseSearchUrl, nil)
-	if e != nil {
-		log.Fatal(e)
-	}
-	request.Header.Add("Authorization-Bearer", authBearer)
-	request.Header.Add("Authorization-Token", authToken)
-	request.Header.Set("Content-Type", "application/json; charset=utf-8")
-
+	api := opendata.NewOpenDataAPI(authBearer, authToken).GetRegistrar()
 	stmt, e := db.Prepare("REPLACE INTO course_list VALUES(?,?,?,?,?,?,?,?)")
 	if e != nil {
 		log.Fatal(e)
 	}
 
 	count := 0
+	iterator := api.SearchCourseSection(nil)
+outer:
 	for retry := 1; retry <= maxRetry; retry++ {
 		if retry != 1 {
 			time.Sleep(waitRetry)
 		} else {
 			time.Sleep(time.Second)
 		}
-		response, e := http.DefaultClient.Do(request)
-		if e != nil {
-			log.Println(e)
-			continue
+		if !iterator.NextPage() {
+			break
 		}
 
-		cs := new(CourseSearch)
-		if e := json.NewDecoder(response.Body).Decode(cs); e != nil {
-			if e := response.Body.Close(); e != nil {
-				log.Println(e)
-			}
-			log.Println(e)
-			continue
-		}
-		if e := response.Body.Close(); e != nil {
-			log.Println(e)
-			continue
-		}
-
-		for _, rawData := range cs.ResultData {
-			data := new(ResultData)
-			if e := json.Unmarshal(rawData, data); e != nil {
-				log.Println(e)
-				continue
+		for i := 0; i < iterator.GetPageSize(); i++ {
+			data := new(opendata.CourseSearchData)
+			err := iterator.GetResult(data, i)
+			if err != nil {
+				log.Print(err)
+				continue outer
 			}
 			insts := make([]string, 0, len(data.Instructors))
 			for _, inst := range data.Instructors {
@@ -109,7 +68,7 @@ func main() {
 				data.Credits,
 				data.Activity,
 				data.Term,
-				([]byte)(rawData),
+				([]byte)(iterator.GetRawData(i)),
 			)
 			if e != nil {
 				log.Println(e)
@@ -117,12 +76,11 @@ func main() {
 				count++
 			}
 		}
-		if cs.ServiceMeta.CurrentPageNumber == cs.ServiceMeta.NumberOfPages {
-			break
-		}
-		request.URL.RawQuery = fmt.Sprintf("page_number=%d", cs.ServiceMeta.NextPageNumber)
 		retry = 0
 		log.Printf("Acquired in total %d listings.", count)
+	}
+	if err := iterator.GetError(); err != nil {
+		log.Print(err)
 	}
 	if e := stmt.Close(); e != nil {
 		log.Fatal(e)
--- course_poll.go
+++ course_poll.go
@@ -2,70 +2,24 @@ package main
 
 import (
 	"bytes"
-	"context"
 	"encoding/json"
 	"log"
 	"net/http"
-	"strings"
 	"time"
 
-	firebase "firebase.google.com/go"
-	"firebase.google.com/go/messaging"
-	"google.golang.org/api/option"
+	"github.com/penn-automate/penn-opendata-api"
 )
 
-type CourseStatus struct {
-	ResultData  []ResultData `json:"result_data"`
-	ServiceMeta ServiceMeta  `json:"service_meta"`
-}
-type ResultData struct {
-	CourseSection        string `json:"course_section"`
-	SectionIDNormalized  string `json:"section_id_normalized"`
-	PreviousStatus       string `json:"previous_status"`
-	Status               string `json:"status"`
-	StatusCodeNormalized string `json:"status_code_normalized"`
-	Term                 string `json:"term"`
-}
+const serverUpdateURL = `https://pennmate.com/notify.php`
 
-const (
-	courseStatusUrl = `https://esb.isc-seo.upenn.edu/8091/open_data/course_status/2019C/all`
-	serverUpdateUrl = `https://pennmate.com/notify.php`
-)
+var statusMap = make(map[string]string)
 
-var (
-	statusMap  = make(map[string]string)
-	messageTtl = time.Minute * 10
-	sendClient *messaging.Client
-)
-
-func sendMessage(course string) {
-	message := &messaging.Message{
-		Android: &messaging.AndroidConfig{
-			Priority: "high",
-			Notification: &messaging.AndroidNotification{
-				Title: course,
-				Body:  "The course opens just now.",
-				Sound: "default",
-			},
-			TTL: &messageTtl,
-		},
-		Topic: strings.ReplaceAll(strings.ReplaceAll(course, "-", ""), " ", "%"),
-	}
-
-	s, err := sendClient.Send(context.Background(), message)
-	if err != nil {
-		log.Println(err)
-	} else {
-		log.Println("Successfully sent message:", s)
-	}
-}
-
-func updateToServer(data ResultData) {
+func updateToServer(data opendata.CourseStatusData) {
 	marshal, e := json.Marshal(data)
 	if e != nil {
 		log.Fatal(e)
 	}
-	request, e := http.NewRequest("POST", serverUpdateUrl, bytes.NewReader(marshal))
+	request, e := http.NewRequest("POST", serverUpdateURL, bytes.NewReader(marshal))
 	if e != nil {
 		log.Fatal(e)
 	}
@@ -77,42 +31,17 @@ func updateToServer(data ResultData) {
 	}
 }
 
-func init() {
-	opt := option.WithCredentialsFile(firebaseJson)
-	app, err := firebase.NewApp(context.Background(), nil, opt)
-	if err != nil {
-		log.Fatalf("error initializing app: %v", err)
-	}
-	sendClient, err = app.Messaging(context.Background())
-	if err != nil {
-		log.Fatal(err)
-	}
-	log.Println("The app has started running...")
-}
-
 func main() {
-	request, e := http.NewRequest("GET", courseStatusUrl, nil)
-	if e != nil {
-		log.Fatal(e)
-	}
-	request.Header.Add("Authorization-Bearer", authBearer)
-	request.Header.Add("Authorization-Token", authToken)
-	request.Header.Set("Content-Type", "application/json; charset=utf-8")
+	log.Println("The app has started running...")
+	api := opendata.NewOpenDataAPI(authBearer, authToken).GetRegistrar()
 	for {
 		time.Sleep(time.Second * 3)
-		response, e := http.DefaultClient.Do(request)
-		if e != nil {
-			log.Println(e)
+		data, err := api.GetAllCourseStatus(term)
+		if err != nil {
+			log.Println(err)
 			continue
 		}
-		cs := new(CourseStatus)
-		if e = json.NewDecoder(response.Body).Decode(cs); e != nil {
-			log.Fatal(e)
-		}
-		if err := response.Body.Close(); err != nil {
-			log.Fatal(err)
-		}
-		for _, data := range cs.ResultData {
+		for _, data := range data {
 			prevStat, ok := statusMap[data.CourseSection]
 			if ok && data.Status != prevStat {
 				log.Printf("Course %s has changed to %s", data.SectionIDNormalized, data.StatusCodeNormalized)
